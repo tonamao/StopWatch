11f309329b5b879cb83ec3c97cf62da82ceb8028
tonamao <46984598+tonamao@users.noreply.github.com>
2019-02-14 12:11:17 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,27 +6,57 @@
   
   var startTime;
   var elapsedTime = 0;
+  var timerId;
+  var pastTime = 0;
+  var isRunning = false;
 
   function updateTimerText() {
     var m = Math.floor(elapsedTime / 60000);
     var s = Math.floor(elapsedTime % 60000 / 1000);
     var ms = elapsedTime % 1000; 
     
+    //adjust the num of digit
+    m = ('0' + m ).slice(-2);
+    s = ('0' + s ).slice(-2);
+    ms = ('00' + ms ).slice(-3);
+
     timer.textContent = m + ':' + s + '.' + ms;
   }
   
 
   function countUp() {
-    setTimeout(function() {
-      elapsedTime = Date.now() - startTime;
-      console.log(elapsedTime);
+    timerId = setTimeout(function() {
+      elapsedTime = Date.now() - startTime + pastTime;
       updateTimerText();
       countUp();
     }, 10);
   }
 
    start.addEventListener('click', function(){
+     if(isRunning){
+       return;
+     }
+     isRunning = true;
      startTime = Date.now();
      countUp();
    });
+
+   stop.addEventListener('click', function(){
+     if(!(isRunning)){
+       return;
+     }
+     isRunning = false;
+     pastTime += Date.now() - startTime;
+     clearTimeout(timerId);
+   });
+
+   reset.addEventListener('click', function(){
+     if(isRunning){
+       return;
+     }
+     elapsedTime = 0;
+     pastTime = 0;
+     updateTimerText();
+   });
+
 })();
