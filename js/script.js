364cb58aa12b2bccdbac37f4de66f8421db97b6b
tonamao <46984598+tonamao@users.noreply.github.com>
2019-02-15 00:20:46 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,62 +1,62 @@
 (function(){
-  var timer = document.getElementById('timer');
-  var start = document.getElementById('start');
-  var stop = document.getElementById('stop');
-  var reset = document.getElementById('reset');
-  
-  var startTime;
-  var elapsedTime = 0;
-  var timerId;
-  var pastTime = 0;
-  var isRunning = false;
+ var timer = document.getElementById('timer');
+ var reset = document.getElementById('reset');
+ var onoff = document.getElementById('onoff');
+ var icon = document.getElementById('icon');
 
-  function updateTimerText() {
-    var m = Math.floor(elapsedTime / 60000);
-    var s = Math.floor(elapsedTime % 60000 / 1000);
-    var ms = elapsedTime % 1000; 
-    
-    //adjust the num of digit
-    m = ('0' + m ).slice(-2);
-    s = ('0' + s ).slice(-2);
-    ms = ('00' + ms ).slice(-3);
+ var startTime;
+ var elapsedTime = 0;
+ var timerId;
+ var pastTime = 0;
+ var isRunning = false;
+ var isTesting = false;
 
-    timer.textContent = m + ':' + s + '.' + ms;
-  }
-  
+ function updateTimerText() {
+   var m = Math.floor(elapsedTime / 60000);
+   var s = Math.floor(elapsedTime % 60000 / 1000);
+   var ms = elapsedTime % 1000;
 
-  function countUp() {
-    timerId = setTimeout(function() {
-      elapsedTime = Date.now() - startTime + pastTime;
-      updateTimerText();
-      countUp();
-    }, 10);
-  }
+   //adjust the num of digit
+   m = ('0' + m ).slice(-2);
+   s = ('0' + s ).slice(-2);
+   ms = ('00' + ms ).slice(-3);
 
-   start.addEventListener('click', function(){
-     if(isRunning){
-       return;
-     }
-     isRunning = true;
-     startTime = Date.now();
-     countUp();
-   });
+   timer.textContent = m + ':' + s + '.' + ms;
+ }
 
-   stop.addEventListener('click', function(){
-     if(!(isRunning)){
-       return;
-     }
-     isRunning = false;
-     pastTime += Date.now() - startTime;
-     clearTimeout(timerId);
-   });
 
-   reset.addEventListener('click', function(){
-     if(isRunning){
-       return;
-     }
-     elapsedTime = 0;
-     pastTime = 0;
+ function countUp() {
+   timerId = setTimeout(function() {
+     elapsedTime = Date.now() - startTime + pastTime;
      updateTimerText();
-   });
+     countUp();
+   }, 10);
+ }
+
+  reset.addEventListener('click', function(){
+    if(isRunning){
+      return;
+    }
+    elapsedTime = 0;
+    pastTime = 0;
+    updateTimerText();
+  });
+
+  onoff.addEventListener('click', function(){
+    //click START
+    if(!(isTesting)){
+      isTesting = true;
+      icon.className = 'far fa-pause-circle';
+      onoff.style.color = '#F27398';
+      startTime = Date.now();
+      countUp();
+    //click STOP
+    } else {
+      isTesting = false;
+      icon.className = 'far fa-play-circle';
+      pastTime += Date.now() - startTime;
+      clearTimeout(timerId);
+    }
+  });
 
 })();
